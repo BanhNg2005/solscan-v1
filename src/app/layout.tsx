781e110ff398b4716fcde8987850e872e4482cb0
Zim <zim@Zims-MacBook-Air.local>
2025-07-09 14:22:36 +0700

    remove Header function and moved it to the Body

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,7 +36,6 @@ export default function RootLayout({ children }: RootLayoutProps) {
             enableSystem
             disableTransitionOnChange
           >
-            <Header />
             <Body />
             <main>
               {children}
