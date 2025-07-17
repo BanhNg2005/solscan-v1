c244a69ddbabf199043890b42a1f957e95e58c24
Zim <zim@Zims-MacBook-Air.local>
2025-07-17 14:09:11 +0700

    Fix Next.js to bind to all interfaces for external access

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: "clone-solscan",
       script: "node_modules/next/dist/bin/next",
-      args: "start -p 3000",
+      args: "start -p 3000 -H 0.0.0.0",
       instances: 2,
       exec_mode: "cluster",
       env: {
